bd1fe6ec99ca72a617bc8a632ffa97f46f703676
umermemon1 <umery101@gmail.com>
2026-05-25 20:23:08 +0500

    Updated

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -62,7 +62,7 @@ export function Hero() {
             </a>
           </motion.div>
           <motion.div whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}>
-            <a href="/src/assets/Umer_Memon_Resume.docx" download>
+            <a href="/src/assets/Umer_Memon_Resume.pdf" download>
               <Button
                 variant="outline"
                 size="lg"
